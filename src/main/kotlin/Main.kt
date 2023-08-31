3c844861151c707a04785fe4cde11a0f9e571342
P1at0nn <131589712+P1at0nn@users.noreply.github.com>
2023-08-31 11:13:58 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ fun main() {
     val kard1 = "visa"
     val kard2 = "vk PAY"
     val kard3 = "mastercard"
-    val transfer = 100000
+    val transfer = 1000
     var transfermonth = 0
     var limit: Boolean
 
@@ -20,50 +20,39 @@ fun main() {
 fun komission(kard: String, transf: Int, transfmonth: Int): Double {
     var komision = 0.0
     when (kard) {
-        "visa", "МИР" -> if ((transf * 0.075) < 35) {
+        "visa", "МИР" -> if ((transf * 0.0075) < 35) {
             komision = 35.0  // рублей
-        } else komision = 0.075  // кофициэнт
+        } else komision = 0.0075  // кофициэнт
         "vk PAY" -> komision = 0.0
         "mastercard", "Maestro " -> if (transfmonth < 75_000) {
             komision = 0.0
-        } else komision = (0.06) + 20
+        } else komision = (0.006) + 20
     }
     return komision
 }
 
 //проверяем на лимиты  если проходит продолжаем
+//!!!!!!!!!!необходимо  учитывать  не только  предыдущие но и текущий
 fun limited(kard: String, transf: Int, transfmonth: Int): Boolean {
     var limit: Boolean = false
     when (kard) {
-        "visa", "МИР", "mastercard", "Maestro " -> if (transf <= 150_000 && transfmonth <= 600_000) {
+        "visa", "МИР", "mastercard", "Maestro " -> if (transf <= 150_000 && transfmonth+transf <= 600_000) {
             limit = true
         } else println("лимит превышен")
 
-        "vk PAY" -> if (transf <= 15_000 && transfmonth <= 40_000) {
+        "vk PAY" -> if (transf <= 15_000 && transfmonth +transf <= 40_000) {
             limit = true
         } else println("лимит превышен")
     }
     return limit
 }
 
-fun calculation(kard: String, transf: Int, transfmonth: Int) {
+fun calculation(kard: String="Vk Pay", transf: Int=3000, transfmonth: Int=0) {
     if (limited(kard, transf, transfmonth) == true) {
         var komision = komission(kard, transf, transfmonth)
-        var result = 0
+        var result : Int
         println("Ваша комиссия  по тарифу $komision ")
-        when (kard) {
-            "visa", "МИР" -> if (komision == 35.0) {
-                result = transf -(transf - komision).toInt()
-
-            } else result = (transf * komision).toInt()
-
-            "vk PAY" -> result = transf
-
-            "mastercard", "Maestro " -> if (komision == 0.0) {
-                result = transf
-            } else result = transf -((transf * 0.06) +20 ).toInt()
-        //непонял как расчитывать (transf -(transf * 0.06)) +20 ) или так
-        }
+        result = (transf - komision).toInt()
         println( "сумма после расчета коммиссии по $kard составит $result ")
     }else println("вы не прошли лимиты")
 
